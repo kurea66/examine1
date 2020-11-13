bfa53d7ea05a64b04efa61fda3a3f242726580b5
kurea <passionately.66.meltdown.4949@gmail.com>
2020-11-13 15:03:00 +0900

    13日分追加

---
+++ Arr2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Arr2{
+	public static void main(String[] args){
+		int[] arr1 = {1,2,3,4,5,6,7,8,9};
+		int[] arr2 = {1,2,3,4,5,6,7,8,9};
+		int[] arr3 = {1,2,3,4,5,6,7,8,9};
+		for(int i=0;i<arr1.length;i++){
+			arr1[new Random().nextInt(10)];
+			arr2[new Random().nextInt(10)];
+			arr3[new Random().nextInt(10)];
+			int temp=arr1;
+		}
+		
+	}
+}
