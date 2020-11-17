2b395ce51780e3bd247b9684209cd9a19157de01
kurea <passionately.66.meltdown.4949@gmail.com>
2020-11-17 15:29:15 +0900

    20201117最終変更

---
+++ Day11/test1.java
@@ -0,0 +1,9 @@
+public class test1{
+	public static void main(String[] args){
+	int[][] arr=new int[3][];
+	arr[0][]=new int[0][3];
+	arr[1][]=new int[1][2];
+	arr[2][]=new int[2][1];
+	System.out.println(java.util.Arrays.toString(arr));
+	}
+}
